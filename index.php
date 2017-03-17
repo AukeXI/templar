b958af43e84d25f83739e14ba77baadcd3c7f93c
Auke H.A. Scheepstra <aukescheepstra1998@gmail.com>
2017-03-17 12:48:02 +0100

    start menu + reset.css

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Geschreven door Auke Scheepstra?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Templar Preview</title>
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php include "menu.php" ?>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php
+// Geschreven door Auke Scheepstra?>
+<ul id="menu-container">
+	<a href="#"><img class="menu-logo" src="img/logo.png"></a>
+	<a href="#"><li class="menu-item">
+		Item 1
+		<ul class="menu-dropdown">
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+		</ul>
+	</li></a>
+	<a href="#"><li class="menu-item">
+		Item 2
+		<ul class="menu-dropdown">
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+		</ul>
+	</li></a>
+	<a href="#"><li class="menu-item">
+		Item 3
+		<ul class="menu-dropdown">
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+			<a href="#"><li class="menu-dropdown-item">Sub-Item</li></a>
+		</ul>
+	</li></a>
+</ul>
